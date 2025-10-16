606cf8feacad73265682575c16ade9b426bd5f3c
Clinton Asamoah <dev.casamoah@EPAHUBB-CONSULT.COM>
2025-10-16 12:50:04 +0100

    changed the newProduct color

--- src/pages/newProduct.jsx
+++ src/pages/newProduct.jsx
@@ -7,35 +7,36 @@ function NewProduct() {
       <div className="create-product">
         <div className="create-product-container">
           <div className="h3">
-            <h3 className="h3"><Link to="/product">Product</Link></h3> <GoChevronRight className="arrowIcon" />
-            <h3 className="h3">Create</h3>
+            <h3 className="router"><Link to="/product" className="router">Product</Link></h3> <GoChevronRight className="arrowIcon" />
+            <h3 className="router">Create</h3>
           </div>
           <h2 className="create-product-header">Create Product</h2>
         </div>
 
         <form className="create-product-form">
-          <div>
+          <div className="form-group1">
             <label className="product-label">Product Name</label>
             <input type="text" placeholder="Enter Product Name" className="product-input" />
           </div>
-          <div>
+          <div className="form-group">
             <label className="product-label">Product Description</label>
             <input type="text" placeholder="Enter Product Description" className="product-input" />
           </div>
-          <div>
+          <div className="form-group">
             <label className="product-label">Amount</label>
             <input type="text" placeholder="Enter Amount" className="product-input" />
           </div>
-          <div>
+          <div className="form-group">
             <label className="product-label">Currency</label>
             <select className="currency-select">
-              <option value="">Select Payment Currency</option>
-              <option value="mpesa">dollar</option>
-              <option value="bank">usd</option>
-              <option value="card">crypto</option>
-              <option value="revolut">Bitcoin</option>
-              <option value="ghs">GHS - Ghana Cedi</option>
-            </select>        </div>
+              <option value="" className="currency-option">Select Payment Currency</option>
+              <option value="mpesa" className="currency-option">dollar</option>
+              <option value="bank" className="currency-option">usd</option>
+              <option value="card" className="currency-option">crypto</option>
+              <option value="revolut" className="currency-option">Bitcoin</option>
+              <option value="ghs" className="currency-option">GHS - Ghana Cedi</option>
+            </select>
+            </div>
         </form>
 
         <div className="btn-group">
